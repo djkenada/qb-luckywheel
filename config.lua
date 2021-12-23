10578e650864024a518142c4c78eeb66a22c03ab
Brian Roberts <37253827+djkenada@users.noreply.github.com>
2021-12-23 14:51:55 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 Config.Vehicle = `coquette4`                              -- Model name for vehicle to win
-Config.Amount = 5000                                      -- Amount of chips needed to spin wheel
-Config.WheelPos = vector3(949.6968, 45.06157, 70.9)       -- Position for wheel
-Config.VehPos = vector4(935.11, 42.61, 71.525, 0.0)       -- Position for vehicle
+Config.Amount = 100                                      -- Amount of chips needed to spin wheel
+Config.WheelPos = vector3(1111.05, 229.85, -50.385)       -- Position for wheel vector3(1111.01, 230.44, -49.64)
+Config.VehPos = vector4(1100.000, 220.000, -50.000, 0.0)       -- Position for vehicle
